283cc5caa2982c4a935c3e6089c4aed8e81a6203
Vinogorich <Vinogorich@gmail.com>
2020-07-11 18:19:54 +0300

    add new freimwork logic

---
+++ src/components/formula/Formula.js
@@ -0,0 +1,9 @@
+import {ExcelComponent} from "../../core/ExcelComponent";
+
+export class Formula extends  ExcelComponent{
+    static className = 'excel__formula'
+
+    toHtml() {
+        return '<h1>Formula</h1>'
+    }
+}
